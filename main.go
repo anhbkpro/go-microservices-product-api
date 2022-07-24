5003b44c30697afddf201ec730f9ea5b60f61e95
Anh Lai <laingocanh1990@gmail.com>
2022-07-24 16:35:05 +0700

    learn about http standard package

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello world")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			//rw.WriteHeader(http.StatusBadRequest)
+			//rw.Write([]byte("Oops"))
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s\n", d)
+	})
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
