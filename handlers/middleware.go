3ca072e5584eb6474de0fc3346e60e2d39b825b5
Anh Lai <laingocanh1990@gmail.com>
2022-07-30 22:00:07 +0700

    add middleware

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -1,1 +1,40 @@
 package handlers
+
+import (
+	"context"
+	"github.com/anhbkpro/go-microservices-product-api/data"
+	"net/http"
+)
+
+func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := &data.Product{}
+
+		err := data.FromJSON(prod, r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// validate the product
+		errs := p.v.Validate(prod)
+		if len(errs) != 0 {
+			p.l.Println("[ERROR] validating product", err)
+
+			// return the validation messages as an array
+			rw.WriteHeader(http.StatusUnprocessableEntity)
+			data.ToJSON(&ValidationError{
+				Messages: errs.Errors(),
+			}, rw)
+			return
+		}
+
+		// add the product to the context
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		req := r.WithContext(ctx)
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(rw, req)
+	})
+}
